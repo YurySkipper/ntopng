083472b4de178d523605a85d0f1885a1b44723f7
yurykovalenko <yury@bytelabs.ru>
2022-03-18 22:24:29 +0300

    commit next portion of translation 18.03.22

--- scripts/locales/ru.lua
+++ scripts/locales/ru.lua
@@ -3897,7 +3897,7 @@ local lang = {
     ["after_behavior_changes"] = "<b>Restart</b> %{product} чтобы сохранить изменения в состоянии поведения.",
     ["after_reset_request"] = "<b>Restart</b> %{product} чтобы сбросить конфигурацию в исходные заводские настройки",
     ["checks"] = "Проверки",
-    ["config_import"] = "Import <span class='item'>{{ type }}</span> Configuration",
+    ["config_import"] = "Импортировать <span class='item'>{{ type }}</span> Конфигурацию",
     ["everything"] = "Entire %{product} configuration (includes users, preferences, and all configurations below)",
     ["export_policy"] = "Export nProbe IPS Policy",
     ["infrastructure_instances"] = "Infrastructure Instances",
